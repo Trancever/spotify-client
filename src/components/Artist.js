5b5e18fd4769626d96dbd3385273bfd5fab14e97
Trancever <dawidu@onet.pl>
2017-12-18 15:12:03 +0100

    Added play button to artist's image.

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -4,14 +4,17 @@ import '../styles/artist.css'
 const Artist = ({ artist }) => {
   return (
     <div className="artist-wrapper">
-      <img
-        src={artist.images[1].url}
-        alt=""
-        height="200"
-        width="200"
-        className="artist-image"
-      />
-      <div className="faded-box" />
+      <div className="image-box">
+        <img
+          src={artist.images[1].url}
+          alt=""
+          height="200"
+          width="200"
+          className="artist-image"
+        />
+        <div className="faded-box" />
+        <div className="play-image-button" />
+      </div>
       <p className="artist-name">{artist.name}</p>
     </div>
   )
